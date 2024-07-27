8b32a3e95d668508425d852617d42757faa53e3d
seongyurim <miruyseong@gmail.com>
2024-07-27 21:42:49 +0900

    Remove folder to replace with submodule

--- shopping-router/src/route/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Detail from '../page/Detail';
-import { Navigate } from  "react-router-dom";
-
-const PrivateRoute = ({authenticate}) => {
-  return authenticate == true ? <Detail /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
