ae78b3ffff22bf5689960c0deefa163a40eb625b
seongyurim <miruyseong@gmail.com>
2024-08-06 23:22:22 +0900

    first commit at 2322

---
+++ redux-practice/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+  
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>Click</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-practice/src/redux/reducer/reducer.js
@@ -0,0 +1,10 @@
+// state는 초기화가 필요하다.
+let initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  
+}
+
+export default reducer;
